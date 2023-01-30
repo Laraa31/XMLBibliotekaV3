66869d03deef67e876d7b69f783a2ea9786e6574
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2023-01-30 12:35:22 +0100

    Add files via upload

---
+++ XMLZadatak/Pretraga.Designer.cs
@@ -0,0 +1,436 @@
+﻿namespace XMLZadatak
+{
+    partial class Pretraga
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNazivKnj = new System.Windows.Forms.TextBox();
+            this.txtImeKor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.btnPretraziKnj = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOIB = new System.Windows.Forms.TextBox();
+            this.btnPretraziKor = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKolicina = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtOIBKorPos = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtDatPos = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtISBNknjPos = new System.Windows.Forms.TextBox();
+            this.btnPsoudi = new System.Windows.Forms.Button();
+            this.btnVrati = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtOIBKorVra = new System.Windows.Forms.TextBox();
+            this.txtDatVra = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtISBNKnjVra = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbIspis
+            // 
+            this.rtbIspis.Location = new System.Drawing.Point(236, 15);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(499, 525);
+            this.rtbIspis.TabIndex = 0;
+            this.rtbIspis.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Naziv knjige";
+            // 
+            // txtNazivKnj
+            // 
+            this.txtNazivKnj.Location = new System.Drawing.Point(28, 79);
+            this.txtNazivKnj.Name = "txtNazivKnj";
+            this.txtNazivKnj.Size = new System.Drawing.Size(172, 20);
+            this.txtNazivKnj.TabIndex = 6;
+            // 
+            // txtImeKor
+            // 
+            this.txtImeKor.Location = new System.Drawing.Point(28, 342);
+            this.txtImeKor.Name = "txtImeKor";
+            this.txtImeKor.Size = new System.Drawing.Size(172, 20);
+            this.txtImeKor.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ime korisnika";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Prezime korisnika";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(28, 399);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(172, 20);
+            this.txtPrezime.TabIndex = 10;
+            // 
+            // btnPretraziKnj
+            // 
+            this.btnPretraziKnj.Location = new System.Drawing.Point(59, 273);
+            this.btnPretraziKnj.Name = "btnPretraziKnj";
+            this.btnPretraziKnj.Size = new System.Drawing.Size(97, 23);
+            this.btnPretraziKnj.TabIndex = 12;
+            this.btnPretraziKnj.Text = "Pretraži knjigu";
+            this.btnPretraziKnj.UseVisualStyleBackColor = true;
+            this.btnPretraziKnj.Click += new System.EventHandler(this.btnPretraziKnj_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "ISBN knjige";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(28, 243);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(172, 20);
+            this.txtISBN.TabIndex = 13;
+            this.txtISBN.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 432);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "OIB korisnika";
+            // 
+            // txtOIB
+            // 
+            this.txtOIB.Location = new System.Drawing.Point(28, 453);
+            this.txtOIB.Name = "txtOIB";
+            this.txtOIB.Size = new System.Drawing.Size(172, 20);
+            this.txtOIB.TabIndex = 15;
+            // 
+            // btnPretraziKor
+            // 
+            this.btnPretraziKor.Location = new System.Drawing.Point(59, 491);
+            this.btnPretraziKor.Name = "btnPretraziKor";
+            this.btnPretraziKor.Size = new System.Drawing.Size(97, 23);
+            this.btnPretraziKor.TabIndex = 18;
+            this.btnPretraziKor.Text = "Pretraži korisnika";
+            this.btnPretraziKor.UseVisualStyleBackColor = true;
+            this.btnPretraziKor.Click += new System.EventHandler(this.btnPretraziKor_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Količina";
+            // 
+            // txtKolicina
+            // 
+            this.txtKolicina.Location = new System.Drawing.Point(28, 191);
+            this.txtKolicina.Name = "txtKolicina";
+            this.txtKolicina.Size = new System.Drawing.Size(172, 20);
+            this.txtKolicina.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Autor";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(28, 136);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(172, 20);
+            this.txtAutor.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(25, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Pretraga";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(762, 15);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 20);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Posuđivanje";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(766, 118);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "OIB Korisnika";
+            // 
+            // txtOIBKorPos
+            // 
+            this.txtOIBKorPos.Location = new System.Drawing.Point(766, 139);
+            this.txtOIBKorPos.Name = "txtOIBKorPos";
+            this.txtOIBKorPos.Size = new System.Drawing.Size(172, 20);
+            this.txtOIBKorPos.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(766, 173);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(99, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Datum posuđivanja";
+            // 
+            // txtDatPos
+            // 
+            this.txtDatPos.Location = new System.Drawing.Point(766, 194);
+            this.txtDatPos.Name = "txtDatPos";
+            this.txtDatPos.Size = new System.Drawing.Size(172, 20);
+            this.txtDatPos.TabIndex = 27;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(766, 61);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(63, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "ISBN knjige";
+            // 
+            // txtISBNknjPos
+            // 
+            this.txtISBNknjPos.Location = new System.Drawing.Point(766, 82);
+            this.txtISBNknjPos.Name = "txtISBNknjPos";
+            this.txtISBNknjPos.Size = new System.Drawing.Size(172, 20);
+            this.txtISBNknjPos.TabIndex = 25;
+            // 
+            // btnPsoudi
+            // 
+            this.btnPsoudi.Location = new System.Drawing.Point(800, 227);
+            this.btnPsoudi.Name = "btnPsoudi";
+            this.btnPsoudi.Size = new System.Drawing.Size(97, 23);
+            this.btnPsoudi.TabIndex = 31;
+            this.btnPsoudi.Text = "Posudi";
+            this.btnPsoudi.UseVisualStyleBackColor = true;
+            // 
+            // btnVrati
+            // 
+            this.btnVrati.Location = new System.Drawing.Point(800, 488);
+            this.btnVrati.Name = "btnVrati";
+            this.btnVrati.Size = new System.Drawing.Size(97, 23);
+            this.btnVrati.TabIndex = 39;
+            this.btnVrati.Text = "Vrati";
+            this.btnVrati.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(766, 379);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(71, 13);
+            this.label13.TabIndex = 38;
+            this.label13.Text = "OIB Korisnika";
+            // 
+            // txtOIBKorVra
+            // 
+            this.txtOIBKorVra.Location = new System.Drawing.Point(766, 400);
+            this.txtOIBKorVra.Name = "txtOIBKorVra";
+            this.txtOIBKorVra.Size = new System.Drawing.Size(172, 20);
+            this.txtOIBKorVra.TabIndex = 37;
+            // 
+            // txtDatVra
+            // 
+            this.txtDatVra.AutoSize = true;
+            this.txtDatVra.Location = new System.Drawing.Point(766, 434);
+            this.txtDatVra.Name = "txtDatVra";
+            this.txtDatVra.Size = new System.Drawing.Size(82, 13);
+            this.txtDatVra.TabIndex = 36;
+            this.txtDatVra.Text = "Datum vraćanja";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(766, 455);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 20);
+            this.textBox5.TabIndex = 35;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(766, 322);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(65, 13);
+            this.label15.TabIndex = 34;
+            this.label15.Text = "Naziv knjige";
+            // 
+            // txtISBNKnjVra
+            // 
+            this.txtISBNKnjVra.Location = new System.Drawing.Point(766, 343);
+            this.txtISBNKnjVra.Name = "txtISBNKnjVra";
+            this.txtISBNKnjVra.Size = new System.Drawing.Size(172, 20);
+            this.txtISBNKnjVra.TabIndex = 33;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label16.Location = new System.Drawing.Point(762, 276);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(80, 20);
+            this.label16.TabIndex = 32;
+            this.label16.Text = "Vraćanje";
+            // 
+            // Pretraga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 552);
+            this.Controls.Add(this.btnVrati);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtOIBKorVra);
+            this.Controls.Add(this.txtDatVra);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.txtISBNKnjVra);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.btnPsoudi);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtOIBKorPos);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtDatPos);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtISBNknjPos);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtKolicina);
+            this.Controls.Add(this.btnPretraziKor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtOIB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.btnPretraziKnj);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImeKor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNazivKnj);
+            this.Controls.Add(this.rtbIspis);
+            this.Name = "Pretraga";
+            this.Text = "Pretraga";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbIspis;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNazivKnj;
+        private System.Windows.Forms.TextBox txtImeKor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Button btnPretraziKnj;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOIB;
+        private System.Windows.Forms.Button btnPretraziKor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKolicina;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtOIBKorPos;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtDatPos;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtISBNknjPos;
+        private System.Windows.Forms.Button btnPsoudi;
+        private System.Windows.Forms.Button btnVrati;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtOIBKorVra;
+        private System.Windows.Forms.Label txtDatVra;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtISBNKnjVra;
+        private System.Windows.Forms.Label label16;
+    }
+}
